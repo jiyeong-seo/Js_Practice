32a0d712b32481731f2e331a3ef619e159d2b1b6
sjy <jen.jyseo@gmail.com>
2022-04-20 23:13:54 +0900

    commit

---
+++ TypeScript/index.ts
@@ -0,0 +1,23 @@
+type Name = string | number;
+
+let 변수명: Name = "문자열";
+
+function 함수(x: number): number {
+  return x * 2;
+}
+함수(2);
+
+type Member = {
+  [key: string]: string;
+};
+let john: Member = { name: "John", age: "123" };
+
+class User {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+const test = new User("문자열");
+
