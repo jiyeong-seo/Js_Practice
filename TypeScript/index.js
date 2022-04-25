b89bbe8e4eedb1ed592a512ddb51868712970dda
sjy <jen.jyseo@gmail.com>
2022-04-25 17:27:48 +0900

    commit

--- TypeScript/index.js
+++ TypeScript/index.js
@@ -11,6 +11,12 @@ var User = /** @class */ (function () {
     return User;
 }());
 var test = new User("문자열");
-var 변수 = 'string';
+var 변수 = "string";
 // 아래의 코드와 동일하다.
 // let 변수 :string = "string";
+// 변수 회원들에 할당될 배열 안에
+// number 또는 string이 요소로 들어올 수 있도록 타입을 지정한 코드.
+var 회원들 = [1, "2", 3];
+// 변수 회원들1에 할당될 값이
+// number이거나, 혹은 string이 요소로 들어오는 배열을 타입으로 지정한 코드.
+var 회원들1 = 1;
