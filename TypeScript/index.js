33fe2e652062c16d482b950796c27bb7abd41388
sjy <jen.jyseo@gmail.com>
2022-04-27 21:26:33 +0900

    commit

--- TypeScript/index.js
+++ TypeScript/index.js
@@ -1,22 +1,33 @@
-var 변수명 = "문자열";
-function 함수(x) {
-    return x * 2;
-}
-함수(2);
-var john = { name: "John", age: "123" };
-var User = /** @class */ (function () {
-    function User(name) {
-        this.name = name;
-    }
-    return User;
-}());
-var test = new User("문자열");
-var 변수 = "string";
+// type Name = string | number;
+// let 변수명: Name = "문자열";
+// function 함수(x: number): number {
+//   return x * 2;
+// }
+// 함수(2);
+// type Member = {
+//   [key: string]: string;
+// };
+// let john: Member = { name: "John", age: "123" };
+// class User {
+//   name: string;
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+// }
+// const test = new User("문자열");
+// let 변수 = "string";
 // 아래의 코드와 동일하다.
 // let 변수 :string = "string";
 // 변수 회원들에 할당될 배열 안에
 // number 또는 string이 요소로 들어올 수 있도록 타입을 지정한 코드.
-var 회원들 = [1, "2", 3];
+// let 회원들: (number | string)[] = [1, "2", 3];
 // 변수 회원들1에 할당될 값이
 // number이거나, 혹은 string이 요소로 들어오는 배열을 타입으로 지정한 코드.
-var 회원들1 = 1;
+// let 회원들1: number | string[] = 1;
+// let 이름 :unknown;
+// 이름 = '';
+// 에러를 발생시킨다.
+// let 변수1 :string = 이름;
+var 나이;
+// number + 1 -> 허용
+나이 + 1;
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,33 +1,43 @@
-type Name = string | number;
+// type Name = string | number;
 
-let 변수명: Name = "문자열";
+// let 변수명: Name = "문자열";
 
-function 함수(x: number): number {
-  return x * 2;
-}
-함수(2);
+// function 함수(x: number): number {
+//   return x * 2;
+// }
+// 함수(2);
 
-type Member = {
-  [key: string]: string;
-};
-let john: Member = { name: "John", age: "123" };
+// type Member = {
+//   [key: string]: string;
+// };
+// let john: Member = { name: "John", age: "123" };
 
-class User {
-  name: string;
-  constructor(name: string) {
-    this.name = name;
-  }
-}
+// class User {
+//   name: string;
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+// }
 
-const test = new User("문자열");
+// const test = new User("문자열");
 
-let 변수 = "string";
+// let 변수 = "string";
 // 아래의 코드와 동일하다.
 // let 변수 :string = "string";
 
 // 변수 회원들에 할당될 배열 안에
 // number 또는 string이 요소로 들어올 수 있도록 타입을 지정한 코드.
-let 회원들: (number | string)[] = [1, "2", 3];
+// let 회원들: (number | string)[] = [1, "2", 3];
 // 변수 회원들1에 할당될 값이
 // number이거나, 혹은 string이 요소로 들어오는 배열을 타입으로 지정한 코드.
-let 회원들1: number | string[] = 1;
+// let 회원들1: number | string[] = 1;
+
+// let 이름 :unknown;
+// 이름 = '';
+
+// 에러를 발생시킨다.
+// let 변수1 :string = 이름;
+
+let 나이: number;
+// number + 1 -> 허용
+나이 + 1;
